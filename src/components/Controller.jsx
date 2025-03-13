ca5c1e7c5e16c13b830c5553a07ef38fe2968631
Jungi Min <jungi@Jungis-Air.lan>
2025-03-12 23:04:37 -0400

    feat: Finish project + State Lifting

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,13 +1,51 @@
 
-const Controller= () => {
-    return <div>
-        <button>-1</button>
-        <button>-10</button>
-        <button>-100</button>
-        <button>+100</button>
-        <button>+10</button>
-        <button>+1</button>
-    </div>
+const Controller= ({onClickButton}) => {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            onClickButton(-1);
+          }}
+        >
+          -1
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(-10);
+          }}
+        >
+          -10
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(-100);
+          }}
+        >
+          -100
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(100);
+          }}
+        >
+          +100
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(10);
+          }}
+        >
+          +10
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(1);
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
 }
 
 export default Controller;
